bb58185a39d04227a693a42e3f9b903171cda993
wellyngton-souza <wellyngtonteixeira12@gmail.com>
2023-04-02 00:12:55 -0300

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace DinoGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dino = new System.Windows.Forms.PictureBox();
+            this.cacto1 = new System.Windows.Forms.PictureBox();
+            this.mainTitle = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.score = new System.Windows.Forms.Label();
+            this.scoreTwo = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dino)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cacto1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dino
+            // 
+            this.dino.Image = ((System.Drawing.Image)(resources.GetObject("dino.Image")));
+            this.dino.Location = new System.Drawing.Point(120, 481);
+            this.dino.Name = "dino";
+            this.dino.Size = new System.Drawing.Size(169, 169);
+            this.dino.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.dino.TabIndex = 0;
+            this.dino.TabStop = false;
+            // 
+            // cacto1
+            // 
+            this.cacto1.Image = ((System.Drawing.Image)(resources.GetObject("cacto1.Image")));
+            this.cacto1.Location = new System.Drawing.Point(805, 531);
+            this.cacto1.Name = "cacto1";
+            this.cacto1.Size = new System.Drawing.Size(150, 150);
+            this.cacto1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.cacto1.TabIndex = 1;
+            this.cacto1.TabStop = false;
+            // 
+            // mainTitle
+            // 
+            this.mainTitle.AutoSize = true;
+            this.mainTitle.Font = new System.Drawing.Font("Consolas", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mainTitle.Location = new System.Drawing.Point(440, 193);
+            this.mainTitle.Name = "mainTitle";
+            this.mainTitle.Size = new System.Drawing.Size(347, 75);
+            this.mainTitle.TabIndex = 2;
+            this.mainTitle.Text = "Dino Game";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.gameTime);
+            // 
+            // score
+            // 
+            this.score.AutoSize = true;
+            this.score.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.score.Location = new System.Drawing.Point(12, 19);
+            this.score.Name = "score";
+            this.score.Size = new System.Drawing.Size(136, 39);
+            this.score.TabIndex = 3;
+            this.score.Text = "Score 0";
+            // 
+            // scoreTwo
+            // 
+            this.scoreTwo.AutoSize = true;
+            this.scoreTwo.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreTwo.Location = new System.Drawing.Point(547, 280);
+            this.scoreTwo.Name = "scoreTwo";
+            this.scoreTwo.Size = new System.Drawing.Size(144, 33);
+            this.scoreTwo.TabIndex = 4;
+            this.scoreTwo.Text = "Score two";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.scoreTwo);
+            this.Controls.Add(this.score);
+            this.Controls.Add(this.dino);
+            this.Controls.Add(this.mainTitle);
+            this.Controls.Add(this.cacto1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.dino)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cacto1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox dino;
+        private System.Windows.Forms.PictureBox cacto1;
+        private System.Windows.Forms.Label mainTitle;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label score;
+        private System.Windows.Forms.Label scoreTwo;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DinoGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool gameStart = false;
+        bool jump = false;
+        bool toFall = false;
+        int scorePoint = 0;
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            //form resize
+            dino.Top = this.Height - dino.Height - 30;
+            cacto1.Top = this.Height - cacto1.Height - 30;
+            cacto1.Left = this.Width - 400;
+
+            mainTitle.Left = (this.Width / 2) - mainTitle.Width / 2;
+            mainTitle.Top = (this.Height / 2) - mainTitle.Height;
+            scoreTwo.Left = (this.Width / 2) - scoreTwo.Width / 2;
+            scoreTwo.Top = (this.Height / 2) - scoreTwo.Height + 30;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cacto1.Left = this.Width - 400;
+        }
+
+        private void gameTime(object sender, EventArgs e)
+        {
+            //cacto move
+            if(gameStart == true){
+                cacto1.Left = cacto1.Left - 30;
+                if (cacto1.Left + cacto1.Width < 0){
+                    cacto1.Left = this.Width;
+                    scorePoint++;
+                    score.Text = "Score " + scorePoint;
+                }
+            }
+            //dino jump
+            if(jump == true && toFall == false){
+                if (dino.Top > this.Height - dino.Height * 2.8){
+                    dino.Top -= 30;
+                }
+                else {
+                    toFall = true;
+                    jump = false;
+                }
+            } else{
+                if (dino.Top < this.Height - dino.Height - 30)
+                {
+                    dino.Top += 15;
+                } else{
+                    toFall = false;
+                }
+            }
+            //colission
+            if(dino.Bounds.IntersectsWith(cacto1.Bounds)){
+                mainTitle.Visible = true;
+                gameStart = false;
+                scoreTwo.Text = "Score " + scorePoint;
+                scoreTwo.Visible = true;
+                cacto1.Left = this.Width;
+                scorePoint = 0;
+                score.Text = "Score " + scorePoint;
+                score.Visible = false;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            //gameStart or keyJump
+            if (gameStart == false){
+                if (e.KeyCode == Keys.Space)
+                {
+                    scoreTwo.Visible = false;
+                    score.Visible = true;
+                    gameStart = true;
+                    mainTitle.Visible = false;
+                }
+            } else{
+                if(jump == false){
+                    jump = true;
+                }
+            }
+        }
+    }
+}
